f70de4e14a4f37837c0f8d594d67a756659a0821
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2025-01-30 02:59:55 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,35 +1,6 @@
-setTimeout(() => {
-    let button = document.evaluate(
-        "//*[@id='root']/div/div/div[1]/div[3]/button[1]", 
-        document, 
-        null, 
-        XPathResult.FIRST_ORDERED_NODE_TYPE, 
-        null
-    ).singleNodeValue;
-
-    if (button) {
-        button.click();
-        console.log("Button clicked successfully!");
-    } else {
-        console.log("Button not found!");
-    }
-}, 3000);
-
-setTimeout(function() {
-    var element = document.evaluate('//*[@id="root"]/div/div[2]/button/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    if (element) {
-        element.click();
-    } else {
-        console.log("Element not found");
-    }
-}, 5000);
-
-setTimeout(function() {
-    var element = document.evaluate('//*[text()="Start creating"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    if (element) {
-        element.click();
-    } else {
-        console.log("Element not found");
-    }
-}, 10000);
-
+var element = document.evaluate('/html/body/main/div/div[2]/div/div[1]/div[3]/div[1]/button[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+if (element) {
+    element.click();
+} else {
+    console.log('Element not found');
+}
