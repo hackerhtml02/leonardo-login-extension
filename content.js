bae2ddcd145245da8aa3aeedf1ed4a71d6e2a530
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2025-01-30 01:43:47 -0800

    Update content.js

--- content.js
+++ content.js
@@ -16,16 +16,6 @@ setTimeout(() => {
 }, 3000);
 
 setTimeout(function() {
-    var element = document.evaluate('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div[1]/form/span/section/div/div/div/div/ul/li[1]/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    if (element) {
-        element.click();
-    } else {
-        console.log("Element not found!");
-    }
-}, 2000); // 2000 milliseconds = 2 seconds
-
-setTimeout(function() {
     var element = document.evaluate('//*[@id="root"]/div/div[2]/button/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     if (element) {
         element.click();
