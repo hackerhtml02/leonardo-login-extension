ffb8a3627889c953053b3d63c194bea5a2275fec
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2025-01-30 00:24:30 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,45 @@
-var buttons = document.querySelectorAll('.VfPpkd-dgl2Hf-ppHlrf-sM5MNb button');
-buttons.forEach(function(button) {
-    if (button.innerText.trim() === "Lanjutkan") {
+setTimeout(() => {
+    let button = document.evaluate(
+        "//*[@id='root']/div/div/div[1]/div[3]/button[1]", 
+        document, 
+        null, 
+        XPathResult.FIRST_ORDERED_NODE_TYPE, 
+        null
+    ).singleNodeValue;
+
+    if (button) {
         button.click();
-        console.log("Button clicked!");
+        console.log("Button clicked successfully!");
+    } else {
+        console.log("Button not found!");
     }
-});
+}, 3000);
+
+setTimeout(function() {
+    var element = document.evaluate('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div[1]/form/span/section/div/div/div/div/ul/li[1]/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    if (element) {
+        element.click();
+    } else {
+        console.log("Element not found!");
+    }
+}, 2000); // 2000 milliseconds = 2 seconds
+
+setTimeout(function() {
+    var element = document.evaluate('//*[@id="root"]/div/div[2]/button/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (element) {
+        element.click();
+    } else {
+        console.log("Element not found");
+    }
+}, 5000);
+
+setTimeout(function() {
+    var element = document.evaluate('//*[text()="Start creating"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (element) {
+        element.click();
+    } else {
+        console.log("Element not found");
+    }
+}, 10000);
+
